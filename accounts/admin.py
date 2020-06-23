733d4dffe0fafa015fad2c3df1921a7ed78892b1
Lucio palmieri <65705360+luciopalm@users.noreply.github.com>
2020-06-23 01:44:24 -0300

    query demos class Tag added

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Custumer)
+admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(Tag)
 admin.site.register(Order)
--- accounts/models.py
+++ accounts/models.py
@@ -3,11 +3,18 @@ from django.db import models
 
 # Create your models here.
 
-class Custumer(models.Model):
+class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
@@ -22,8 +29,12 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -33,9 +44,11 @@ class Order(models.Model):
         ('Delivered', 'Delivered')
     )
 
-
-
-    #custumer =
-    #product =
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_crated = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+
+
+
